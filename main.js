27d161031a569a36e64761216b32aa47c2bfd027
Jared Hanson <jaredhanson@gmail.com>
2013-06-15 16:23:17 -0700

    Emit authenticate events.

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 define(['./lib/dom',
         'gadgets.rpc',
         'querystring',
+        'events',
         'class'],
-function(dom, rpc, qs, clazz) {
+function(dom, rpc, qs, Emitter, clazz) {
   
   var RELAY_READY_CHANNEL = 'oauth2relayReady';
   var CALLBACK_CHANNEL = 'oauth2callback';
@@ -27,13 +28,36 @@ function(dom, rpc, qs, clazz) {
    */
   function Provider(opts) {
     opts = opts || {};
+    Emitter.call(this);
     this.name = 'google';
     this._authorizationURL = opts.authorizationURL || 'https://accounts.google.com/o/oauth2/auth';
     this._relayURL = opts.relayURL || 'https://accounts.google.com/o/oauth2/postmessageRelay';
     this._clientID = opts.clientID;
     this._scope = opts.scope;
-    this._responseType = opts.responseType || 'token';
+    //this._responseType = opts.responseType || 'token';
+    this._responseType = opts.responseType || 'code token id_token';
     this._relayID = undefined;
+  }
+  
+  /**
+   * Inherit from `Emitter`.
+   */
+  clazz.inherits(Provider, Emitter);
+  
+  /**
+   * Initiate a login attempt.
+   *
+   * In contrast to a standard OAuth 2.0 flow, using the postmessage profile
+   * will not trigger a redirect back to the invoking application.  Instead, the
+   * result will be delivered via the relay iframe.
+   *
+   * @return {Provider}
+   * @api public
+   */
+  Provider.prototype.login = function() {
+    var authUrl = this._getAuthUrl();
+    this.redirect(authUrl);
+    return this;
   }
   
   /**
@@ -85,18 +109,39 @@ function(dom, rpc, qs, clazz) {
       console.log(authUrl);
       
       // TODO: Make the initial immediate mode check optional.
-      
       dom.openHiddenFrame(authUrl, 'immediate-auth-frame');
     }
     
     function onCallback(result) {
-      console.log('!!! OAUTH2 CALLBACK');
-      console.log(result);
+      var resp = result.slice(1); // omit the leading `#`
+      var params = qs.parse(resp)
+        , id = { provider: 'google.com' }
+        , creds;
+      
+      console.log(params)
+      
+      // TODO: parse id_token
+      
+      if (params.access_token) {
+        creds = creds || {};
+        creds.type = params.token_type || 'bearer';
+        creds.accessToken = params.access_token;
+        creds.expiresIn = params.expires_in;
+        creds.code = params.code;
+      }
+      
+      self.emit('authenticate', id, creds);
     }
     
     return this;
   }
   
+  /**
+   * Build authorization URL, optionally `immediate`.
+   *
+   * @return {Provider}
+   * @api public
+   */
   Provider.prototype._getAuthUrl = function(immediate) {
     var query = {
       response_type: this._responseType,
